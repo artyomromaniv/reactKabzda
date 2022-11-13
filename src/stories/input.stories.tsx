0cc79fd7cd4a245fce5d6b605c62b8f9fa68f8dc
artyomromaniv <romaniv.artyom8@gmail.com>
2022-11-13 13:47:43 +0300

    storybook add conrolled input,checkbox, select

--- src/stories/input.stories.tsx
+++ src/stories/input.stories.tsx
@@ -1,6 +1,6 @@
 import React, {ChangeEvent, useRef, useState} from 'react';
 import {ComponentStory, ComponentMeta} from '@storybook/react';
-
+import {action} from "@storybook/addon-actions";
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
@@ -12,15 +12,6 @@ export default {
     },
 } as ComponentMeta<typeof input>;
 
-// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-//const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
-
-// export const Primary = Template.bind({});
-// // More on args: https://storybook.js.org/docs/react/writing-stories/args
-// Primary.args = {
-//   primary: true,
-//   label: 'Button',
-// };
 
 export const UncontrolledInput = () => <input/>
 
@@ -45,5 +36,32 @@ export const GetValueOfUncontrolledInputByButtonPress = () => {
     return <><input ref={inputRef}/><button onClick={save}>save</button> - actual value: {value} </>
 }
 
+export const ControlledInput = () => {
+    const[parentValue,setParentValue]=useState('')
+    const onChangeHandler = (e:ChangeEvent<HTMLInputElement>)=> {
+        setParentValue(e.currentTarget.value)
+    }
+    return <input value={parentValue} onChange={onChangeHandler}/>
+}
+
+export const ControlledCheckbox = () => {
+    const[parentValue,setParentValue]=useState(true)
+    const onChangeHandler = (e:ChangeEvent<HTMLInputElement>)=> {
+        setParentValue(e.currentTarget.checked)
+    }
+    return <input type="checkbox"  checked={parentValue} onChange={onChangeHandler}/>
+}
+export const ControlledSelect = () => {
+    const[parentValue,setParentValue]=useState<string | undefined>(undefined)
+    const onChangeHandler = (e:ChangeEvent<HTMLSelectElement>)=> {
+        setParentValue(e.currentTarget.value)
+    }
+    //если option, то всегда value(стринга)
+    return <select value={parentValue} onChange={onChangeHandler}>
+        <option value={'1'}>Minsk</option>
+        <option value={'2'}>Moscow</option>
+        <option value={'3'}>Kiev</option>
+    </select>
+}
 
 export const ControlledInputWithFixedValue = () => <input value={'It-incubator.by'}/>
