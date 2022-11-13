4644334ab427ec83c383ea0f6c33f6fafd53c226
artyomromaniv <romaniv.artyom8@gmail.com>
2022-11-13 15:59:46 +0300

    storybook add Accordion with items inside props

--- src/components/accordion/Accordion.stories.tsx
+++ src/components/accordion/Accordion.stories.tsx
@@ -18,15 +18,38 @@ export default {
 const Template: ComponentStory<typeof Accordion> = (args) => <Accordion {...args} />;
 
 const callback = action("Accordion mode changed")
+const onClickCallback = action("Some item was clicked")
 
-export const MenuCollapsedMode = () => <Accordion collapsed={true} onChange={callback} titleValue={"Menu"} />
-export const MenuUnCollapsedMode = () => <Accordion collapsed={false} onChange={callback} titleValue={"Menu"} />
 
-export const UsersCollapsedMode = () => <Accordion collapsed={true} onChange={callback} titleValue={"Users"} />
-export const UsersUnCollapsedMode = () => <Accordion collapsed={false} onChange={callback} titleValue={"Users"} />
+export const MenuCollapsedMode = () => <Accordion collapsed={true}
+                                                  onChange={callback}
+                                                  titleValue={"Menu"}
+                                                  items={[]}
+                                                  onClick={onClickCallback}/>
+export const MenuUnCollapsedMode = () => <Accordion collapsed={false}
+                                                    onChange={callback}
+                                                    titleValue={"Menu"}
+                                                    items={[
+                                                        {title:"Dimych", value: 1},
+                                                        {title:"Artyom", value: 2},
+                                                        {title:"Nadya", value: 3}
+                                                    ]}
+                                                    onClick={onClickCallback}
+/>
+
+// export const UsersCollapsedMode = () => <Accordion collapsed={true} onChange={callback} titleValue={"Users"} />
+// export const UsersUnCollapsedMode = () => <Accordion collapsed={false} onChange={callback} titleValue={"Users"} />
 
 
 export const ModeChanging = () => {
     const [value,setValue] = useState(false)
-    return <Accordion collapsed={value} onChange={()=>setValue(!value)} titleValue={"Users"} />
+    return <Accordion collapsed={value}
+                      onChange={()=>setValue(!value)}
+                      titleValue={"Users"}
+                      items={[
+                          {title:"Dimych", value: 1},
+                          {title:"Artyom", value: 2},
+                          {title:"Nadya", value: 3}
+                      ]}
+                      onClick={onClickCallback}/>
 }
--- src/components/accordion/Accordion.tsx
+++ src/components/accordion/Accordion.tsx
@@ -1,9 +1,16 @@
 import React from "react";
 
+type ItemType = {
+    title:string
+    value:any
+}
+
 type AccordionPropsType = {
     titleValue: string
     collapsed: boolean
     onChange: () => void
+    items: ItemType[]
+    onClick:(value:any)=>void
 }
 
 export function Accordion(props: AccordionPropsType) {
@@ -12,7 +19,7 @@ export function Accordion(props: AccordionPropsType) {
     return (
         <div>
             <AccordionTitle onChange={props.onChange} title={props.titleValue}/>
-            {!props.collapsed && <AccordionBody/>}
+            {!props.collapsed && <AccordionBody items={props.items} onClick={props.onClick}/>}
         </div>
     )
 }
@@ -29,14 +36,17 @@ function AccordionTitle(props: AccordionTitlePropsType) {
     )
 }
 
-function AccordionBody() {
+type AccordionBodyPropsType = {
+    items: ItemType[]
+    onClick:(value:any)=>void
+}
+
+function AccordionBody(props:AccordionBodyPropsType) {
     console.log("AccordionBody rendering")
     return (
         <div>
             <ul>
-                <li>1</li>
-                <li>2</li>
-                <li>3</li>
+                {props.items.map((i,index) =>  <li onClick={()=>{props.onClick(i.value)}} key={index}>{i.title}</li>)}
             </ul>
         </div>
     )
