4718ae38c7398f15917bcf4714462ade73be2d96
artyomromaniv <romaniv.artyom8@gmail.com>
2022-12-01 13:37:14 +0300

    add навороченный useState

---
+++ src/stories/UseState.stories.tsx
@@ -0,0 +1,25 @@
+import React, {useState} from "react";
+
+export default {
+    title: 'useState demo',
+}
+
+ function generateData() {
+    console.log("generate Data")
+    return 1
+}
+
+export const Example1 = () => {
+    console.log("Example1")
+    const [counter, setCounter] = useState(generateData)
+
+    const changer = (state: number) =>{
+        return state + 1
+    }
+
+
+    return <>
+        <button onClick={() => setCounter(changer)}>+</button>
+        {counter}
+    </>
+}
