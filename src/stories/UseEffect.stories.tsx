6c08759668e7c0a219be1db605ef24d2f16e30d0
artyomromaniv <romaniv.artyom8@gmail.com>
2022-12-04 03:44:18 +0300

    clear timeout or interval in useEffect

--- src/stories/UseEffect.stories.tsx
+++ src/stories/UseEffect.stories.tsx
@@ -1,4 +1,6 @@
 import React, {useEffect, useState} from "react";
+import {log} from "util";
+import {clearInterval} from "timers";
 
 export default {
     title: 'useEffect demo',
@@ -24,12 +26,12 @@ export const SimpleExample = () => {
     useEffect(() => {
         console.log("useEffect only firs render (аналог componentDidMount")
         document.title = counter.toString()
-    },[])
+    }, [])
 
     useEffect(() => {
         console.log("useEffect first render every counter(deps) changes")
         document.title = counter.toString()
-    },[counter])
+    }, [counter])
 
     return <>
         Hello, {fake}
@@ -45,11 +47,11 @@ export const SetTimeoutExample = () => {
     console.log("SetTimeoutExample")
 
     useEffect(() => {
-        setTimeout(()=> {
+        setTimeout(() => {
             console.log("setTimeout")
             document.title = counter.toString()
-        },1000)
-    },[counter])
+        }, 1000)
+    }, [counter])
 
     return <>
         Hello, {fake}
@@ -65,15 +67,42 @@ export const SetIntervalExample = () => {
     console.log("SetIntervalExample")
 
     useEffect(() => {
-        setInterval(()=> {
-            setCounter((state) => state +1)
-        },1000)
-    },[])
+        const intervalId = setInterval(() => {
+            setCounter((state) => state + 1)
+        }, 1000)
+//важно! всегда зачищать интервал или таймаут
+        return () => {
+            clearInterval(intervalId)
+        }
+    }, [])
 
     return <>
-        Hello, fake:  {fake}
+        Hello, fake: {fake}
         {/*<button onClick={() => setFake(fake + 1)}> fake +</button>*/}
         {/*<button onClick={() => setCounter(counter + 1)}> counter+</button>*/}
-        counter:  {counter}
+        counter: {counter}
+    </>
+}
+
+
+export const KeysTrackerExample = () => {
+    const [text, setText] = useState('');
+    console.log('Component rendered');
+
+    useEffect(() => {
+        const handler = (e:KeyboardEvent) => {
+            console.log(e.key)
+            setText(text + e.key)
+        }
+        window.addEventListener('keypress', handler);
+
+        return () => {
+            window.removeEventListener('keypress', handler)
+        }
+
+    }, [text]);
+
+    return <>
+        Typed text: {text}
     </>
 }
